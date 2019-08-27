ab3bb42f487336304643d6189302c345c064566b
GoofFucker <47990107+GoofFucker@users.noreply.github.com>
2019-08-27 23:15:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,20 +8,10 @@ client.on("ready", () => {
     client.user.setActivity(`Le meilleur bot pour Pub !`, { type: "STREAMING", url: "https://www.twitch.tv/balancetonquoi19" })
 });
 
-client.on('message', message => {
-
-    if (message.content.startsWith('>Pub')) {
-        let text = message.content.slice('>Pub'.length); // cuts off the /private part
-        message.delete();
-        //if(message.channel.id === "608036987571535893") {
-        const publ = new Discord.RichEmbed()
-                    .setColor(0xCF40FA)
-                    .setAuthor(`${message.author.tag}`, `${message.author.avatarURL} `)    
-                    .addField(`✅ Votre message à bien été envoyé !`, `Toujours prêt à servir.`)
-                message.channel.send({
-                    embed: publ
+client.on('guildMemberAdd', member => {
+    member.send("Hey mec, n'hésite pas à aller faire un tour sur ma nouvelle vidéo : https://www.youtube.com/watch?v=CGyQ_2l3Chs");
                 });
 
-                message.guild.members.forEach(member => { member.send(text);
-    });
-}});
+client.on('guildMemberRemove', member => {
+    member.send("Hey mec, n'hésite pas à aller faire un tour sur ma nouvelle vidéo : https://www.youtube.com/watch?v=CGyQ_2l3Chs");
+                });

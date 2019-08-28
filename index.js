ca296417ddb9c5000f67a519a06e351e566d7b02
GoofFucker <47990107+GoofFucker@users.noreply.github.com>
2019-08-28 20:06:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,5 @@ client.login(process.env.CAITLIN);
 
 client.on("ready", () => {
     console.log("Ready");
-    client.user.setActivity(`Le meilleur bot pour Pub !`, { type: "STREAMING", url: "https://www.twitch.tv/balancetonquoi19" })
+    client.user.setActivity(`Un fan de LouMusiqueOfficiel`, { type: "STREAMING", url: "https://www.twitch.tv/neathofficiel" })
 });
-
-client.on('guildMemberAdd', member => {
-    member.send("Hey mec, n'hésite pas à aller faire un tour sur ma nouvelle vidéo : https://www.youtube.com/watch?v=CGyQ_2l3Chs");
-                });
-
-client.on('guildMemberRemove', member => {
-    member.send("Hey mec, n'hésite pas à aller faire un tour sur ma nouvelle vidéo : https://www.youtube.com/watch?v=CGyQ_2l3Chs");
-                });
